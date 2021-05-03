73d0dabff97155cf6880c6828427195224862dea
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-03 10:34:01 +0700

    Update ThotamBuddyServiceProvider.php

--- src/ThotamBuddyServiceProvider.php
+++ src/ThotamBuddyServiceProvider.php
@@ -16,7 +16,7 @@ class ThotamBuddyServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'thotam-buddy');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'thotam-buddy');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
