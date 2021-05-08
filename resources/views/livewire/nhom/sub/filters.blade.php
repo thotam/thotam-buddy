5976b16b7ce23c2a36bd25ca6b8e0814677d7d0e
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-08 09:37:07 +0700

    Update filters.blade.php

--- resources/views/livewire/nhom/sub/filters.blade.php
+++ resources/views/livewire/nhom/sub/filters.blade.php
@@ -2,14 +2,6 @@
 <div class="px-4 pt-4 mb-0" wire:ignore>
     <div class="form-row">
 
-        @if ($hr->can("add-buddy"))
-            <div class="col-md-auto mb-4 pr-md-3">
-                <label class="form-label d-none d-md-block">&nbsp;</label>
-                <div class="col px-0 mb-1 text-md-left text-center">
-                    <button type="button" class="btn btn-success waves-effect" wire:click.prevent="add_buddy" wire:loading.attr="disabled" thotam-blockui><span class="fas fa-plus-circle mr-2"></span>Đăng ký</button>
-                </div>
-            </div>
-        @endif
 
     </div>
 </div>
