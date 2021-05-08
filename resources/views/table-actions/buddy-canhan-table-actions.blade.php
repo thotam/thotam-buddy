98fc799de8634aa734e22d4bb1ceba8f11de6f83
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-08 12:09:06 +0700

    fix actions button

--- resources/views/table-actions/buddy-canhan-table-actions.blade.php
+++ resources/views/table-actions/buddy-canhan-table-actions.blade.php
@@ -1,29 +1,29 @@
 <div class='action-div icon-4 px-0 mx-1 d-flex justify-content-around text-center'>
 
-    <div class='col action-icon-w-50 action-icon' thotam-livewire-method='view_buddy' thotam-model-id='{{ $id }}'><i class='text-info fas fa-search'></i></div>
+    <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='view_buddy' thotam-model-id='{{ $id }}'><i class='text-info fas fa-search'></i></div>
 
     @if ($trangthai_id == 5)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='edit_buddy' thotam-model-id='{{ $id }}'><i class='text-indigo fas fa-edit'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='edit_buddy' thotam-model-id='{{ $id }}'><i class='text-indigo fas fa-edit'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if (($trangthai_id == 9 || $trangthai_id == 11) && $nguoihuongdan_of_buddy_ids->contains($id))
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='len_tieuchi_buddy' thotam-model-id='{{ $id }}'><i class='text-orange fas fa-bullseye'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='len_tieuchi_buddy' thotam-model-id='{{ $id }}'><i class='text-orange fas fa-bullseye'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if (($trangthai_id == 19 || $trangthai_id == 21) && $isMine)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='baocao_buddy' thotam-model-id='{{ $id }}'><i class='text-success fas fa-file-upload'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='baocao_buddy' thotam-model-id='{{ $id }}'><i class='text-success fas fa-file-upload'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if ($trangthai_id == 5)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='delete_buddy' thotam-model-id='{{ $id }}'><i class='text-danger fas fa-trash-alt'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='delete_buddy' thotam-model-id='{{ $id }}'><i class='text-danger fas fa-trash-alt'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
 
--- resources/views/table-actions/buddy-nhom-table-actions.blade.php
+++ resources/views/table-actions/buddy-nhom-table-actions.blade.php
@@ -1,35 +1,35 @@
 <div class='action-div icon-4 px-0 mx-1 d-flex justify-content-around text-center'>
 
-    <div class='col action-icon-w-50 action-icon' thotam-livewire-method='view_buddy' thotam-model-id='{{ $id }}'><i class='text-info fas fa-search'></i></div>
+    <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='view_buddy' thotam-model-id='{{ $id }}'><i class='text-info fas fa-search'></i></div>
 
     @if ($trangthai_id == 5)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='edit_buddy' thotam-model-id='{{ $id }}'><i class='text-indigo fas fa-edit'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='edit_buddy' thotam-model-id='{{ $id }}'><i class='text-indigo fas fa-edit'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if ($trangthai_id == 5 || $trangthai_id == 7 || $trangthai_id == 9)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='duyet_buddy' thotam-model-id='{{ $id }}'><i class='text-success fas fa-check-double'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='duyet_buddy' thotam-model-id='{{ $id }}'><i class='text-success fas fa-check-double'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if ($trangthai_id == 13)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='len_tieuchi_buddy' thotam-model-id='{{ $id }}'><i class='text-orange fas fa-bullseye'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='len_tieuchi_buddy' thotam-model-id='{{ $id }}'><i class='text-orange fas fa-bullseye'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if ($trangthai_id == 23)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='danhgia_buddy' thotam-model-id='{{ $id }}'><i class='text-linux far fa-check-square'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='danhgia_buddy' thotam-model-id='{{ $id }}'><i class='text-linux far fa-check-square'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
     @if ($trangthai_id == 5)
-        <div class='col action-icon-w-50 action-icon' thotam-livewire-method='delete_buddy' thotam-model-id='{{ $id }}'><i class='text-danger fas fa-trash-alt'></i></div>
+        <div class='col-1 action-icon-w-50 action-icon' thotam-livewire-method='delete_buddy' thotam-model-id='{{ $id }}'><i class='text-danger fas fa-trash-alt'></i></div>
     @else
-        <div class='col action-icon-w-50'></div>
+        <div class='col-1 action-icon-w-50'></div>
     @endif
 
 
