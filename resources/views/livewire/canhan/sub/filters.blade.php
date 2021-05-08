da6d4ab3978ae6af56a201fa9acaf3dc0ec07435
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-08 09:37:05 +0700

    Update filters.blade.php

--- resources/views/livewire/canhan/sub/filters.blade.php
+++ resources/views/livewire/canhan/sub/filters.blade.php
@@ -2,7 +2,7 @@
 <div class="px-4 pt-4 mb-0" wire:ignore>
     <div class="form-row">
 
-        @if ($hr->can("add-buddy"))
+        @if ($hr->can("add-buddy") || $hr->is_thanhvien || $hr->is_quanly)
             <div class="col-md-auto mb-4 pr-md-3">
                 <label class="form-label d-none d-md-block">&nbsp;</label>
                 <div class="col px-0 mb-1 text-md-left text-center">
